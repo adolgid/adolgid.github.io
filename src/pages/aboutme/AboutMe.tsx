ed25abd40b811a17ca256fd58a9259074660f307
Juan Gidoni <juan.gidoni@gmail.com>
2024-11-28 12:23:36 +0100

    fix: change

--- src/pages/aboutme/AboutMe.tsx
+++ src/pages/aboutme/AboutMe.tsx
@@ -11,14 +11,14 @@ export default function AboutMe() {
     <>
       <div className="flex">
         <div className="w-full flex items-center justify-between bg-white">
-          <div className="w-1/2 p-6 md:p-3">
+          <div className="w-1/2 p-3 md:p-3">
             <img
               src={images[0]}
               className="w-full md:aspect-[2/2]"
               alt="Draw 1"
             />
           </div>
-          <div className="w-1/2 p-6 md:p-3">
+          <div className="w-1/2 p-3 md:p-3">
             <img
               src={images[1]}
               className="w-full md:aspect-[2/2]"
@@ -29,26 +29,26 @@ export default function AboutMe() {
       </div>
       <div className="flex">
         <div className="w-full md:w-1/2 flex flex-col items-center justify-between bg-white">
-          <div className="max-w-full p-6 md:p-12">
+          <div className="max-w-full p-3 md:p-12">
             <a
               href="mailto:adolgidoni@gmail.com"
-              className="text-3xl md:text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 hover:text-slate-500 mb-4 md:mb-6 font-hoves-extended-extralight"
+              className="text-2xl md:text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 hover:text-slate-500 mb-4 md:mb-3 font-hoves-extended-extralight"
             >
               Contact
             </a>
           </div>
         </div>
         <div className="w-full md:w-1/2 flex flex-col items-center justify-between bg-white">
-          <div className="max-w-full p-6 md:p-12">
+          <div className="max-w-full p-3 md:p-12">
             <AudioPlayer audioSrc={music} text="Play" />
           </div>
         </div>
         <div className="w-full md:w-1/2 flex flex-col items-center justify-between bg-white">
-          <div className="max-w-full p-6 md:p-12">
+          <div className="max-w-full p-3 md:p-12">
             <a
               href="https://paypal.me/juangidoni"
               target="_BLANK"
-              className="text-3xl md:text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 hover:text-slate-500 mb-4 md:mb-6 font-hoves-extended-extralight"
+              className="text-2xl md:text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 hover:text-slate-500 mb-4 md:mb-3 font-hoves-extended-extralight"
             >
               Invest
             </a>
@@ -56,7 +56,7 @@ export default function AboutMe() {
         </div>
       </div>
 
-      <div className="w-full flex-col items-center justify-center bg-white px-6 md:px-12 py-4 text-center md:text-left font-hoves-regular text-xl"></div>
+      <div className="w-full flex-col items-center justify-center bg-white px-3 md:px-12 py-4 text-center md:text-left font-hoves-regular text-xl"></div>
     </>
   );
 }
