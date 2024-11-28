f1ac461488cceff566aae804f08b4a363cc670d4
Juan Gidoni <juan.gidoni@gmail.com>
2024-11-28 11:28:27 +0100

    fix: font sizes

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -13,7 +13,7 @@ export default function Landing() {
       <div className="flex">
         <div className="w-full md:w-1/2 flex flex-col items-center justify-between bg-white">
           <div className="max-w-full p-6 md:p-12">
-            <h1 className="text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 mb-4 md:mb-6 font-hoves-extended-extralight">
+            <h1 className="text-3xl md:text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 mb-4 md:mb-6 font-hoves-extended-extralight">
               Creador de contenido digital. Experto en síntesis sensorial.
               Improvisador desde 1993
               <br></br>
@@ -38,7 +38,7 @@ export default function Landing() {
             <a
               href="https://paypal.me/juangidoni"
               target="_BLANK"
-              className="text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 hover:text-slate-500 mb-4 md:mb-6 font-hoves-extended-extralight"
+              className="text-3xl md:text-5xl font-wrap md:text-5xl lg:text-7xl font-bold text-slate-800 hover:text-slate-500 mb-4 md:mb-6 font-hoves-extended-extralight"
             >
               Invest
             </a>
@@ -47,13 +47,13 @@ export default function Landing() {
       </div>
 
       <div className="w-full flex-col items-center justify-center bg-white px-6 md:px-12 py-4">
-        <h2 className="font-hoves-extended-extralight text-4xl text-center md:text-left">
+        <h2 className="font-hoves-extended-extralight text-2xl text-center md:text-left">
           Adolfo Nilson Gidoni
         </h2>
       </div>
 
       <div className="w-full flex-col items-center justify-center bg-white px-6 md:px-12 py-4">
-        <h2 className="font-hoves-regular text-4xl text-center md:text-left">
+        <h2 className="font-hoves-regular text-2xl text-center md:text-left">
           adolgidoni@gmail.com
         </h2>
       </div>
